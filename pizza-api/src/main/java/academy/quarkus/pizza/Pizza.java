3495086257c44f2095b9a441039211b20451773e
Murillo Oliveria <murilloooliveira12@gmail.com>
2024-05-22 17:21:29 +0000

    testes

--- pizza-api/src/main/java/academy/quarkus/pizza/Pizza.java
+++
@@ -1,9 +0,0 @@
-package academy.quarkus.pizza;
-
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-import jakarta.persistence.Entity;
-
-@Entity
-public class Pizza extends PanacheEntity {
-    public String description;
-}
